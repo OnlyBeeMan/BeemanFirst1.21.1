5bbbf5cd8ad7828ef0a9b206b40fad82848fa358
OnlyBeeMan <lucasmarques4c@gmail.com>
2026-02-15 19:41:51 +0000

    The Initial Commit

---
+++ src/main/java/net/beeman/beemanfirst/BeemanFirstClient.java
@@ -0,0 +1,10 @@
+package net.beeman.beemanfirst;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeemanFirstClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/beeman/beemanfirst/BeemanFirstDataGenerator.java
@@ -0,0 +1,11 @@
+package net.beeman.beemanfirst;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeemanFirstDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
